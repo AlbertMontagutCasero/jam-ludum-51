bf1fc2f4d646d2bc35d0a3538245bc6a0072887c
Albert Montagut Casero <albert.montagut.casero@gmail.com>
2022-10-01 13:52:58 +0200

    feat: update add command view

--- Assets/_Project/Infrastructure/View/AddCommandsView.cs
+++ Assets/_Project/Infrastructure/View/AddCommandsView.cs
@@ -15,21 +15,37 @@ namespace LudumDare51.Infrastructure
         {
             var getCommandConfigurationInteractor = InteractorServiceLocator.GetInstance().GetService<GetCommandConfigurationInteractor>();
             var commandConfiguration = getCommandConfigurationInteractor.GetCommandsConfiguration();
-
+            
+            
             for (var i = 0; i < commandConfiguration.Count; i++)
             {
                 var currentCommandConfiguration = commandConfiguration[i];
                 var addCommandButtonView = Instantiate(this.addCommandButtonViewPrefab, this.transform);
-                ((RectTransform)addCommandButtonView.transform).localPosition = Vector3.right * i * this.GetButtonSize();
+                var buttonRectTransform = ((RectTransform) addCommandButtonView.transform); 
+                var buttonPosition = Vector3.zero;
+                buttonPosition.x = -(this.GetWidth() * 0.5f);
+                buttonPosition.x += i * this.GetOffsetBetweenButtons();
+                buttonRectTransform.localPosition = buttonPosition;
 
                 addCommandButtonView.SetConfiguration(currentCommandConfiguration);
                 this.addCommandButtonViews.Add(addCommandButtonView);
             }
         }
 
+        private float GetWidth()
+        {
+            return ((RectTransform)this.transform).rect.width;
+        }
+
         private float GetButtonSize()
         {
             return 96;
+        }
+        
+        private float GetOffsetBetweenButtons()
+        {
+            var offsetBetweenButtons = 16;
+            return this.GetButtonSize() + offsetBetweenButtons;
         }
     }
 }
